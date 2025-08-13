60419a39f0aebc3d808bcbd54e53bf8d8c297b05
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-13 23:00:06 +0530

    Backend setup

---
+++ QuickCourt-Backend/src/main/java/org/example/quickcourtbackend/QuickCourtBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.quickcourtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCourtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCourtBackendApplication.class, args);
+    }
+
+}
---
+++ QuickCourt-Backend/src/test/java/org/example/quickcourtbackend/QuickCourtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quickcourtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCourtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
