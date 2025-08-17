2896720e13cf2d71ee3a92c8e653a642fd29733c
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-17 21:40:18 +0530

    testing

--- Frontend/src/utils/axios.jsx
+++ Frontend/src/utils/axios.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1",
+  baseURL:"https://quick-court-1-230n.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
